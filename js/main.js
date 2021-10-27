15dc0583d33b5a77569ab30e1e6b52f455325279
Mykola Pererbenesiuk <sanistray@gmail.com>
2021-10-27 20:23:37 +0300

    ке

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var userInfo = {
 
 setTimeout(() => {
     $.ajax({
-        url: 'https://exo-portfolio-server.herokuapp.com/',
+        url: 'https://exo-portfolio-bot.herokuapp.com/new_user',
         method: 'get',
         dataType: 'json',
         async: false,
